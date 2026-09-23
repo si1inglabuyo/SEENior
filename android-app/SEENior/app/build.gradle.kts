ae0cba94c385564c04c33fee46fdab3825802302
si1inglabuyo <ocasionreviman@gmail.com>
2026-09-24 02:15:14 +0800

    Bump to versionCode 15 / v1.14

--- android-app/SEENior/app/build.gradle.kts
+++ android-app/SEENior/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.pup.seenior"
         minSdk = 26
         targetSdk = 37
-        versionCode = 14
-        versionName = "1.13"
+        versionCode = 15
+        versionName = "1.14"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
